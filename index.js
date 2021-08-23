b05d5f1df124fcb6f65351013bade41b1b63679b
pavi <pavithirandxd@gmail.com>
2021-08-23 15:05:23 +0530

    final review

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const { mongoose } = require("./db.js");
 var InfoController = require("./controllers/info.js");
 var ExperienceController = require("./controllers/experience.js");
 
+const port = process.env.PORT || 8080;
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:4200" }));
-app.listen(3000, () => console.log("server started at port 3000"));
+
+// app.use(cors({ origin: "http://localhost:4200" }));
+// app.listen(3000, () => console.log("server started at port 3000"));
+
+app.use(cors());
+app.listen(port, () => console.log("server started at port 3000"));
+
 
 app.use("/info", InfoController);
 app.use("/experience", ExperienceController);
