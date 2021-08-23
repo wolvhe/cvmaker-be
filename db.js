0007b77d1114b49d6d0593889472816a587f587d
pavi <pavithirandxd@gmail.com>
2021-08-23 15:01:14 +0530

    final changes

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://neon:neon@cluster0.ero8t.mongodb.net/cvmaker', (err) => {
-    if(!err)
-        console.log("mongodb connection success");
-    else
-        console.log("connection failed : ", err);
-});
-
-// mongoose.connect('mongodb://localhost:27017/cvmaker', (err) => {
+// mongoose.connect('mongodb+srv://neon:neon@cluster0.ero8t.mongodb.net/cvmaker', (err) => {
 //     if(!err)
 //         console.log("mongodb connection success");
 //     else
 //         console.log("connection failed : ", err);
 // });
+
+mongoose.connect('mongodb://localhost:27017/cvmaker', (err) => {
+    if(!err)
+        console.log("mongodb connection success");
+    else
+        console.log("connection failed : ", err);
+});
 
 module.exports = mongoose;
